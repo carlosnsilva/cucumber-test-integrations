3ed31b211f84e5b7247581b6ef23e2ed5476f063
carlosnsilva <cmanoel17@gmail.com>
2023-05-14 16:34:02 -0300

    Subindo implementacoes nem todas as camadas

--- src/main/java/com/stackspot/cucumber/integration/controller/ClienteController.java
+++ src/main/java/com/stackspot/cucumber/integration/controller/ClienteController.java
@@ -1,4 +1,38 @@
 package com.stackspot.cucumber.integration.controller;
 
+import com.stackspot.cucumber.integration.model.Cliente;
+import com.stackspot.cucumber.integration.service.ClienteService;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/clientes")
 public class ClienteController {
+
+    @Autowired
+    private ClienteService service;
+
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<Cliente> getAll(){
+        return service.getClientes();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public Cliente getCLient(@NotNull @PathVariable("id") Long id){
+        return service.getCliente(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Cliente createClient(Cliente client){
+        return service.saveCliente(client);
+    }
+
+
 }
--- src/main/java/com/stackspot/cucumber/integration/service/ClienteService.java
+++ src/main/java/com/stackspot/cucumber/integration/service/ClienteService.java
@@ -1,4 +1,27 @@
 package com.stackspot.cucumber.integration.service;
 
+import com.stackspot.cucumber.integration.model.Cliente;
+import com.stackspot.cucumber.integration.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    public List<Cliente> getClientes(){
+        return this.repository.findAll();
+    }
+
+    public Cliente getCliente(Long id){
+        return this.repository.findById(id).orElse(null);
+    }
+
+    public Cliente saveCliente(Cliente client){
+        return this.repository.save(client);
+    }
 }
