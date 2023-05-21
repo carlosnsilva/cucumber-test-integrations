a2989d889d23cf9ad762be6f9114c13fb4c24e6b
carlosnsilva <cmanoel17@gmail.com>
2023-05-21 20:17:13 -0300

    Fix: Ajustes na estrutura de pacotes

--- src/test/java/com/stackspot/cucumber/integration/integration/setup/CucumberSpringConfiguration.java
+++ src/test/java/com/stackspot/cucumber/integration/integration/setup/CucumberSpringConfiguration.java
@@ -3,10 +3,12 @@ package com.stackspot.cucumber.integration.integration.setup;
 import com.stackspot.cucumber.integration.Application;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 @CucumberContextConfiguration
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ContextConfiguration(initializers = {SpringBootContextInitializer.class})
+@ActiveProfiles("test")
 public class CucumberSpringConfiguration {
 }
